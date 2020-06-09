11807ac9471e259cb5f36af8e7183da830c23f2c
dem585 <65715072+dem585@users.noreply.github.com>
2020-06-09 23:09:10 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+	$('.heade__top-call-btn').on('click',function(){
+		$('.menu-z').addClass('active');
+	});
+
+	$('.center-info-btn').on('click',function(){
+		$('.menu-z').addClass('active');
+	});
+
+	$('.menu-z-close-btn').on('click',function(){
+		$('.menu-z').removeClass('active');
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
